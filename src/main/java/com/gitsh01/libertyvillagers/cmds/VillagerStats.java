28e7f38713ba7bd0912dbef279352ef3eb9002d8
shodan <mega@ioexception.at>
2024-06-15 16:00:13 +0200

    Fix villager stats

--- src/main/java/com/gitsh01/libertyvillagers/cmds/VillagerStats.java
+++ src/main/java/com/gitsh01/libertyvillagers/cmds/VillagerStats.java
@@ -6,6 +6,8 @@ import com.gitsh01.libertyvillagers.LibertyVillagersServerInitializer;
 import com.mojang.brigadier.context.CommandContext;
 import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
 import net.fabricmc.loader.api.FabricLoader;
+import net.minecraft.component.ComponentMap;
+import net.minecraft.component.DataComponentTypes;
 import net.minecraft.component.type.WrittenBookContentComponent;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.ai.brain.MemoryModuleType;
@@ -91,6 +93,8 @@ public class VillagerStats {
                 true
         );
 
+        bookStack.set(DataComponentTypes.WRITTEN_BOOK_CONTENT, bookContent);
+
         if (LibertyVillagersMod.isClient()) {
             LibertyVillagersClientInitializer.openBookScreen(bookStack);
         } else if (FabricLoader.getInstance().getModContainer("server_translations_api").isPresent()) {
@@ -116,13 +120,13 @@ public class VillagerStats {
             linesRemaining--;
             if (linesRemaining <= 0) {
                 linesRemaining = LINES_PER_PAGE;
-                pageTags.add(RawFilteredPair.of(Text.of("\"" + curString + "\"")));
+                pageTags.add(RawFilteredPair.of(Text.of(curString.toString())));
                 curString = new StringBuilder();
             }
         }
 
         if (!curString.isEmpty()) {
-            pageTags.add(RawFilteredPair.of(Text.of("\"" + curString + "\"")));
+            pageTags.add(RawFilteredPair.of(Text.of(curString.toString())));
         }
 
         return pageTags;
--- src/main/java/com/gitsh01/libertyvillagers/mixin/FishingBobberEntityRendererMixin.java
+++ src/main/java/com/gitsh01/libertyvillagers/mixin/FishingBobberEntityRendererMixin.java
@@ -13,8 +13,10 @@ import net.minecraft.entity.passive.VillagerEntity;
 import net.minecraft.entity.projectile.FishingBobberEntity;
 import net.minecraft.util.math.MathHelper;
 import net.minecraft.util.math.RotationAxis;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -22,6 +24,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(FishingBobberEntityRenderer.class)
 public abstract class FishingBobberEntityRendererMixin extends EntityRenderer<FishingBobberEntity> {
 
+    @Final
     @Shadow
     private static RenderLayer LAYER;
 
@@ -34,6 +37,7 @@ public abstract class FishingBobberEntityRendererMixin extends EntityRenderer<Fi
         return 0;
     }
 
+    @Unique
     private static void renderFishingLineAsLine(float x, float y, float z, VertexConsumer buffer,
                                                 MatrixStack.Entry matrices, float segmentStart, float segmentEnd) {
         float f = x * segmentStart;
@@ -57,6 +61,7 @@ public abstract class FishingBobberEntityRendererMixin extends EntityRenderer<Fi
         }
     }
 
+    @Unique
     private static void vertex(VertexConsumer buffer, MatrixStack.Entry matrix, int light, float x, int y, int u, int v) {
         buffer.vertex(matrix, x - 0.5f, (float) y - 0.5f, 0.0f)
                 .color(255, 255, 255, 255)
